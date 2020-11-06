f6f67b485dec4d172d0c97cf8208d39a3d7dddc3
hillary26121 <hillaryywallace@gmail.com>
2020-11-06 14:30:12 -0500

    start of game

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-console.log("here");
 const wordBank = require("./word-bank.json");
-console.log(wordBank[0]);
 const prompt = require('readline-sync');
-const answer = prompt.question("may i have your name");
-console.log(answer);
+
+let startGame= prompt.question('Welcome to hangman! To exit the game: Press ctrl+c ');
+console.log(startGame);
+let userName = prompt.question('May I have your name please? ');
+
+
+while(!userName){
+   userName = prompt.question('You have to enter a name Silly!! ');
+}
+console.log(`Hey ${userName}! Thanks for playing. Let's get started. \nEnter your first letter!`)
+
+let chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+let dashes = "";
+
+chosenWord.split('').forEach( (character)=>{
+    dashes = `${dashes} _ `
+});
+console.log(dashes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (/[a-zA-Z]/.test(userInput)) {
+//     // Do something in here
+//     console.log("This is a letter");
+//   } else if (/[^a-zA-Z]/.test(userInput)) {
+//     // Do something here
+//     console.log("This is NOT a letter");
+//   }
