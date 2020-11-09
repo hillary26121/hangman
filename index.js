ea9a7eddbec3e6b627f432f78e212e70af3b3253
hillary26121 <hillaryywallace@gmail.com>
2020-11-09 09:37:21 -0500

    added some requirements

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
-// const wordBank = require("./word-bank.json");
-const wordBank = ['apple'];
+const wordBank = require("./word-bank.json");
+// const wordBank = ['apple'];
 const prompt = require('readline-sync');
-
-let startGame= prompt.question('Welcome to hangman! To exit the game: Press ctrl+c ');
-console.log(startGame);
-let userName = prompt.question('May I have your name please? ');
+const hangman = ['O', '\|', '/', '\\', '\_', '_'];
 
 
-while(!userName){
-   userName = prompt.question('You have to enter a name Silly!! ');
-}
-console.log(`Hey ${userName}! Thanks for playing. Let's get started. \nEnter your first letter!`)
+    let startGame= prompt.question('Welcome to hangman! To exit the game: Press ctrl+c ');
+    console.log(startGame);
+    let userName = prompt.question('May I have your name please? ');
 
-let chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-let dashes = [];
 
-chosenWord.split('').forEach( (character)=>{
-    dashes.push(`_`); 
+    while(!userName){
+      userName = prompt.question('You have to enter a name Silly!! ');    
+    }
+  
+    
+const hangmanGame = () => {  
+    console.log(`\nHey ${userName}! Thanks for playing. Let's get started. \nEnter your first letter!`)
+    let chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    let dashes = [];
+
+    chosenWord.split('').forEach( (character)=>{
+        dashes.push(`_`); 
    
-});
-console.log(dashes);
+    });
+    console.log(dashes);
 
-//loop begins
-let i = 0;
-//   while( dashes.includes("_") || i < 7){ 
-    while(dashes.includes("_") && i < 7){ 
+
+   
+    let guessedLetters = [];
+    let incorrectGuesses = 0;
+
+    while(dashes.includes("_") && incorrectGuesses < 6){ 
     let userGuess = prompt.question("");
     let guessPosition = chosenWord.indexOf(userGuess);
+
     if(guessPosition === -1){
+       
         console.log(`No ${userName}, ${userGuess} is not a letter. Try again`);
-        i++;
+        if(!guessedLetters.includes(userGuess)){
+            incorrectGuesses++;
+        }
     } else {
-        //Set letter
         dashes = dashes.map( (dashOrLetter, index)=>{
             if(chosenWord[index] === userGuess){
             return chosenWord[index];
@@ -40,13 +49,38 @@ let i = 0;
             }
         })
       
-      
-    //join and display dashes to user
-        console.log(dashes.join(' ') );
     }
+    guessedLetters.push(userGuess);
+    console.log(dashes.join(' ') );
   }
 
+roundsPlayed++;
+if(incorrectGuesses >= 6){
+    console.log(`You lose ${userName}, the answer was "${chosenWord}". \nYou're kind of dumb. Try again to get smarter.`);
+} else{
+    roundsWon++;
+    console.log("Congratulations!! You're smarter than most 3 year olds.")
+}
 
+
+
+
+}
+
+let roundsPlayed = 0;
+let roundsWon = 0;
+
+while(true){
+    hangmanGame();
+    console.log(`Games played: ${roundsPlayed} \nGames won: ${roundsWon}`);
+    let StartNewGame = prompt.question('To start a new game, press enter. To exit, press ctrl + c')
+    console.log(StartNewGame);  
+    
+}
+
+
+//needs multiple rounds
+//case insensitive, has to be a letter
 
 
 
